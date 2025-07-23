84ef70b73291c9857a14d3b1e2b68c36c2c0e23d
baiyuechuu <taitran411789@gmail.com>
2025-07-23 23:20:52 +0700

    add: tailwind and shadcn ui lib

--- Website/src/App.tsx
+++ Website/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
 	return (
-		<>
-			<p>Hello</p>
-		</>
+		<div className="flex min-h-svh flex-col items-center justify-center">
+			<Button>Click me</Button>
+		</div>
 	);
 }
 
